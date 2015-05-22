1537c2142899e87d6cc16948486a29d6c96dde8a
moohdyy <martin@leichtzumerken.net>
2015-05-22 15:43:58 +0200

    save status.

--- js/googleInterface.js
+++ js/googleInterface.js
@@ -1,4 +1,28 @@
+var newsSearch;
+
+
 function searchAPI(string){
 	console.log( "whaaaa "+ string);
+	newsSearch.execute(string);
 	
+}
+
+
+
+
+function myIP() {
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+    xmlhttp.send();
+
+    hostipInfo = xmlhttp.responseText.split("\n");
+
+    for (i=0; hostipInfo.length >= i; i++) {
+        ipAddress = hostipInfo[i].split(":");
+        if ( ipAddress[0] == "IP" ) return ipAddress[1];
+    }
+
+    return false;
 }
